b1b66a7949f9ac279636eaf070346b772310a07c
Louis Maddox <louismmx@gmail.com>
2025-08-06 15:02:51 +0100

    feat: get datatype schemas correct

--- process.py
+++ process.py
@@ -1,7 +1,8 @@
 import orjson
 import polars as pl
 
-df = pl.read_parquet("*.parquet")
+# df = pl.read_parquet("*.parquet")
+df = pl.read_parquet("data/*.parquet")
 
 
 def unpivot_from_struct_col(struct_col: str, struct_field: str, var_name: str):
@@ -71,10 +72,67 @@ def unpivot_struct(
     return new_frame
 
 
+# Some entity IDs inferred to be str dtype datavalue from parser at:
+# https://github.com/maxlath/wikibase-sdk/blob/2447f7d1355d3461adb2676b0c5336534932feda/src/types/snakvalue.ts#L68
+
+str_snak_values = [
+    "commonsMedia",  # str datavalue
+    "geo-shape",  # str datavalue
+    "tabular-data",  # str datavalue
+    "url",  # str datavalue
+    "external-id",  # str datavalue
+    "wikibase-property",  # str datavalue
+    "monolingualtext",  # str datavalue
+    "string",  # str datavalue
+    "musical-notation",  # str datavalue
+    "math",  # str datavalue
+    "entity-schema",  # str datavalue
+    "wikibase-lexeme",  # str datavalue
+    "wikibase-form",  # str datavalue
+    "wikibase-sense",  # str datavalue
+]
+struct_snak_values = {
+    "wikibase-item": [
+        "id",
+        "labels",  # categorical
+    ],
+    "globe-coordinate": [
+        "latitude",
+        "longitude",
+        "altitude",
+        "precision",
+        "globe",
+    ],
+    "quantity": [
+        "amount",
+        "unit",
+        "upperBound",
+        "lowerBound",
+        "unit-labels",
+    ],
+    "time": [
+        "time",
+        "timezone",
+        "before",
+        "after",
+        "precision",
+        "calendarmodel",
+    ],
+}
+struct_categoricals = {
+    "wikibase-item": {"labels": ["wikibase-item-label-lang", "wikibase-item-label"]},
+    "quantity": {"unit-labels": []},
+}
+
+goal = "wikibase-item"
+dts = {}
+
 all_claims = []
 for entity_id, claims_dict in zip(df.get_column("id"), claims_decoded):
     for property_key, claims_list in claims_dict.items():
         for claim in claims_list:
+            if claim["mainsnak"]["datatype"] != goal:
+                continue
             claim_frame = pl.DataFrame(
                 {
                     "id": entity_id,
@@ -83,37 +141,52 @@ for entity_id, claims_dict in zip(df.get_column("id"), claims_decoded):
                     "rank": claim["rank"],
                 }
             )
-            unpacked_dv_claims = claim_frame.select(
-                [
-                    "id",
-                    "property",
-                    pl.col("datavalue")
-                    .struct.rename_fields(["datavalue-id", "datavalue-labels"])
-                    .struct.unnest(),
-                    "datatype",
-                    "property-labels",
-                    "rank",
-                ]
-            )
-            dv_unpivotted_claims = unpivot_struct(
-                frame=unpacked_dv_claims,
-                struct_field="datavalue-labels",
-                pivot_var="datavalue-label-lang",
-                pivot_val="datavalue-label",
-            )
+            if "datavalue" not in claim_frame:
+                continue  # invalid entry
+            dv_fr = claim_frame["datavalue"].struct.unnest()
+            dt = dv_fr["labels"].dtype
+            dt_str = str(dt).split("(")[0]
+            dts.setdefault(dt_str, 0)
+            dts[dt_str] += 1
+            if dts[dt_str] == 1:
+                print(dv_fr)
+            continue
+            scalar_dv = claim_frame["datatype"].item() in str_snak_values
+            if scalar_dv:
+                dv_unpivotted_claims = claim_frame
+            else:
+                unpacked_dv_claims = claim_frame.select(
+                    [
+                        "id",
+                        "property",
+                        pl.col("datavalue")
+                        .struct.rename_fields(["datavalue-id", "datavalue-labels"])
+                        .struct.unnest(),
+                        "datatype",
+                        "property-labels",
+                        "rank",
+                    ]
+                )
+                dv_unpivotted_claims = unpivot_struct(
+                    frame=unpacked_dv_claims,
+                    struct_field="datavalue-labels",
+                    pivot_var="datavalue-label-lang",
+                    pivot_val="datavalue-label",
+                )
             prop_unpivotted_claims = unpivot_struct(
                 frame=dv_unpivotted_claims,
                 struct_field="property-labels",
                 pivot_var="property-label-lang",
                 pivot_val="property-label",
             )
-            all_claims.append(
-                prop_unpivotted_claims.filter(
+            if scalar_dv:
+                final_claim_df = prop_unpivotted_claims
+            else:
+                final_claim_df = prop_unpivotted_claims.filter(
                     pl.col("datavalue-label-lang") == pl.col("property-label-lang")
                 )
-            )
+            all_claims.append(final_claim_df)
+            break
 
 claims_df = pl.concat(all_claims)
-claims_normalized = claims_df.with_columns(pl.col("mainsnak").struct.unnest()).drop(
-    "mainsnak"
-)
+claims_normalized = claims_df
